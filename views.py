02306406b05b90f61dc66265af9fece3014f2893
Jaroslav Lhotak <lhotakj@gmail.com>
2024-01-08 22:03:40 +0100

    heartbeat

--- views.py
+++ views.py
@@ -11,3 +11,8 @@ def sample_api():
     if request.method == "POST":
         data.append(request.get_json())
     return json.dumps(data)
+
+@App.route("/heartbeat", methods=["GET"])
+def heartbeat():
+    if request.method == "GET":
+        return "OK"
