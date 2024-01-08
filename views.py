37e64a0e4d666926e112193f878c1da47585a175
Jaroslav Lhotak <lhotakj@gmail.com>
2024-01-08 22:07:10 +0100

    ok

--- views.py
+++ views.py
@@ -1,16 +1,10 @@
 from Cors import App
 from flask import request
-import json
 
-data = []
-
-
-# modified
-@App.route("/", methods=["GET", "POST"])
-def sample_api():
-    if request.method == "POST":
-        data.append(request.get_json())
-    return json.dumps(data)
+@App.route("/", methods=["GET"])
+def heartbeat():
+    if request.method == "GET":
+        return "Hello world!"
 
 @App.route("/heartbeat", methods=["GET"])
 def heartbeat():
