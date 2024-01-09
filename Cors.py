a59a500b0de4bc16c3f4976958648d00f5446106
Jaroslav Lhotak <lhotakj@gmail.com>
2024-01-09 23:03:57 +0100

    revert

--- Cors.py
+++ Cors.py
@@ -2,7 +2,9 @@ from flask import Flask
 from flask_cors import CORS
 from whitenoise import WhiteNoise
 
+# App = Flask(__name__)
+# cors = CORS(App)
+# App.wsgi_app = WhiteNoise(App.wsgi_app, root="app/", prefix="/")
+
 App = Flask(__name__)
 cors = CORS(App)
-App.wsgi_app = WhiteNoise(App.wsgi_app  # , root="app/", prefix="/"
-                          )
