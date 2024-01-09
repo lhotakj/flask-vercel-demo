34662df13ccffd26a5a85fc9aba0bcd689c6109d
Jaroslav Lhotak <lhotakj@gmail.com>
2024-01-09 22:30:04 +0100

    whitenoise

--- Cors.py
+++ Cors.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask_cors import CORS
+from whitenoise import WhiteNoise
 
 App = Flask(__name__)
 cors = CORS(App)
+App.wsgi_app = WhiteNoise(App.wsgi_app, root="static/")
